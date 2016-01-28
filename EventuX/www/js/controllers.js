bca995b04b16695025cf039478107b5c5c8839ca
babieste <barbaraeste01@gmail.com>
2016-01-28 12:26:32 +0000

    api foursquare funfando

--- EventuX/www/js/controllers.js
+++ EventuX/www/js/controllers.js
@@ -12,8 +12,29 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('buscarEventuXCtrl', function($scope) {
-
+.controller('buscarEventuXCtrl', function($scope, $http) {
+    $scope.buscafour = function(userloc, param){
+            //vars da url
+            var url = "https://api.foursquare.com/v2/venues/search";
+            var cid = "?client_id=N2NQDXGZP4MIA31OAELTJVE4QHX4W2SFDMOLB1ONNQNJFYYM";
+            var cse = "&client_secret=HKXD2BGFHMRLEV4BW55MMOLFSHXWWS25RSNW5SESXOJ1D5SY";
+            var ver = "&v=20130815";
+            var loc = "&near=";
+            var userlocal = window.encodeURIComponent(userloc);
+            var que = "&query=";
+            var par = window.encodeURIComponent(param);
+            //concatenacao da url       
+            var url_get = url + cid + cse + ver + loc + userlocal + que + par;
+            //execucao da url
+    $http.get(url_get).then(function successCallback(response) {
+                console.log(response);
+                $scope.result = response.data.response.venues;
+                //console.log(response.venues);
+              }, function errorCallback(response) {
+                alert(JSON.stringify(response));
+            });
+    
+    };
 })
    
 .controller('resultadosCtrl', function($scope) {
